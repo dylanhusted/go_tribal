0a12e8e2a5ff5074ce00088b32e707a4b7606f22
Dylan Husted <dhusted1@babson.edu>
2014-09-12 21:34:42 -0400

    First push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mondaypickmeup::Application.config.secret_key_base = '85addbc116dad9f5d59e9c384615860ccd9ced28f3aaf874e0831f9b719d18c77b1a1c09e5a25ef7bde47b2a16f6b48d952e450fba20cfc3c76073b28fda32ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mondaypickmeup::Application.config.session_store :cookie_store, key: '_mondaypickmeup_session'
